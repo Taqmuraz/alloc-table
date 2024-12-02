791f22de63921f1f238953487897b79e3131f494
lulook <lulook23@gmail.com>
2024-12-02 11:53:26 +0100

    c and java tests

---
+++ c/alloc.c
@@ -0,0 +1,42 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include <sys/time.h>
+
+long long time_ms()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    long long ms = tv.tv_sec * 1000LL + tv.tv_usec / 1000;
+    return ms;
+}
+
+int run (int r, int s) {
+  long long start = time_ms();
+  int e = 0;
+  for(int i = 0; i < r; i++) {
+    int* p = (int*)malloc(sizeof(int) * s);
+    e += p[0];
+  }
+  long long end = time_ms();
+  long long took = end - start;
+  printf("Took : %lld ms\n", took);
+  return took;
+}
+
+int main(int argc, char** argv) {
+  int r = atoi(argv[1]);
+  int s = atoi(argv[2]);
+  printf("Inputs : %d %d\n", r, s);
+  long long sum = 0;
+  long long min = 1L << 32;
+  long long max = -1;
+  int ts = 10;
+  for(int i = 0; i < ts; i++) {
+    long long t = run(r, s);
+    if(t < min) min = t;
+    if(t > max) max = t;
+    sum += t;
+  }
+  double avg = ((double)sum) / ts;
+  printf("Min : %lld, max : %lld, average : %f\n", min, max, avg);
+}
---
+++ java/Alloc.java
@@ -0,0 +1,31 @@
+
+public class Alloc {
+  static long run (int r, int s) {
+    long start = System.currentTimeMillis();
+    int e = 0;
+    for (int i = 0; i < r; i++) {
+      int[] b = new int[s];
+      e += b[0];
+    }
+    long end = System.currentTimeMillis();
+    long took = end - start;
+    System.out.printf("Time took : %d ms\n", took);
+    return took;
+  }
+  public static void main (String[] args) {
+    int r = Integer.parseInt(args[0]);
+    int s = Integer.parseInt(args[1]);
+    long sum = 0;
+    long min = Long.MAX_VALUE;
+    long max = -1;
+    int ts = 10;
+    for (int i = 0; i < ts; i++) {
+      long t = run(r, s);
+      sum += t;
+      if(t < min) min = t;
+      if(t > max) max = t;
+    }
+    double avg = ((double)sum) / ts;
+    System.out.printf("Min : %d, max : %d, average : %f\n", min, max, avg);
+  }
+}
